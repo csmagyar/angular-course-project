b895035328f823adfb7bc43ab04a222765ddcee9
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-03-07 21:55:26 +0100

    bug fix

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,24 +10,26 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>()
 
-  private recipes: Recipe[] = [
-    new Recipe('Stuffed cabbage',
-    'It tastes even better the next day (and the day after that).',
-    'https://images.ctfassets.net/314o13npeir2/01mpLWpl4CcnVCTBhK95UM/c7940aa2e3c2b01224461876ce3839c9/hungarian-stuffed-cabbage-toltott-kaposzta.JPG?w=620&q=80&fm=webp',
-    [
-      new Ingredient('sour cabbage leaves', 10),
-      new Ingredient('pork meat', 500),
-      new Ingredient('rice', 100)
-    ]),
-    new Recipe('Goulash',
-    'The traditional side to the goulash is egg dumplings.',
-    'https://images.ctfassets.net/314o13npeir2/SiNChikHmTDXYwrTlk5Ut/1e99cc57c0a773d2e6109ff1630f7826/hungarian-beef-goulash-marhaporkolt.JPG?w=620&fl=progressive&q=80',
-    [
-      new Ingredient('beef meat', 800),
-      new Ingredient('sweet paprika', 3),
-      new Ingredient('onion', 2)
-    ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('Stuffed cabbage',
+  //   'It tastes even better the next day (and the day after that).',
+  //   'https://images.ctfassets.net/314o13npeir2/01mpLWpl4CcnVCTBhK95UM/c7940aa2e3c2b01224461876ce3839c9/hungarian-stuffed-cabbage-toltott-kaposzta.JPG?w=620&q=80&fm=webp',
+  //   [
+  //     new Ingredient('sour cabbage leaves', 10),
+  //     new Ingredient('pork meat', 500),
+  //     new Ingredient('rice', 100)
+  //   ]),
+  //   new Recipe('Goulash',
+  //   'The traditional side to the goulash is egg dumplings.',
+  //   'https://images.ctfassets.net/314o13npeir2/SiNChikHmTDXYwrTlk5Ut/1e99cc57c0a773d2e6109ff1630f7826/hungarian-beef-goulash-marhaporkolt.JPG?w=620&fl=progressive&q=80',
+  //   [
+  //     new Ingredient('beef meat', 800),
+  //     new Ingredient('sweet paprika', 3),
+  //     new Ingredient('onion', 2)
+  //   ])
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) { }
 
