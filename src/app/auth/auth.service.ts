eceb65f2bb48621b50e28d1d6fd4a385fcecdfe2
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-03-16 20:13:01 +0100

    add login functionality

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email:	string;
   refreshToken:	string;
   expiresIn:	string;
   localId:	string;
+  registered?: boolean;
 }
 
 @Injectable({
@@ -24,18 +25,34 @@ export class AuthService {
       email: email,
       password: password,
       returnSecureToken: true
-    }).pipe(catchError(errorRes => {
-      let errorMessage = 'An unknown error occurred!'
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(() => new Error(errorMessage));
-      }
-      switch(errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = "The email address is already in use by another account.";
-          break;
-      }
+    }).pipe(catchError(this.handleError));
+  }
 
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCf4sLxSI58XfOrxslwU_xykxC0f6ctzLA',{
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }).pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!'
+    if (!errorRes.error || !errorRes.error.error) {
       return throwError(() => new Error(errorMessage));
-    }));
+    }
+    switch(errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = "The email address is already in use by another account.";
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = "There is no user record corresponding to this identifier. The user may have been deleted.";
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = "The password is invalid or the user does not have a password.";
+        break;
+    }
+
+    return throwError(() => new Error(errorMessage));
   }
 }
