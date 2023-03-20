3c7cec4fd220233305c88fae8578b085b9f25829
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-03-20 20:05:33 +0100

    create core module for providers

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -5,8 +5,6 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 
-
-
 @NgModule({
   declarations: [
     ShoppingListComponent,
