42094515020dab9c24784c7f60180b7e30656351
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-03-20 20:29:35 +0100

    outsource auth into a separate module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AuthLoggedinGuard } from "./auth/auth-loggedin.guard";
-import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'auth', component: AuthComponent, canActivate: [AuthLoggedinGuard] },
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' }
 ]
 
 @NgModule({
