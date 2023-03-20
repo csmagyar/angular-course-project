14a87c6444ee0c801f31b2f1b811bada84b216d8
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-03-20 19:57:47 +0100

    create a shared module

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -13,10 +13,10 @@ import { FormsModule } from '@angular/forms';
     ShoppingEditComponent
   ],
   imports: [
-    CommonModule,
     FormsModule,
     RouterModule.forChild([ 
-      { path: 'shopping-list', component: ShoppingListComponent }])
+      { path: 'shopping-list', component: ShoppingListComponent }]),
+    SharedModule
   ]
 })
 export class ShoppingListModule { }
