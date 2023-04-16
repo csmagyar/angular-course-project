e350f0bb1e590bcfcf19fcd9cc2166c446e6f815
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-04-16 14:31:16 +0200

    implement state management for add ingredients
    This commit implements state management for adding ingredients using the @ngrx/store library. The state management pattern helps in managing application state in a more organized and centralized way, making it easier to manage complex state changes and maintain a single source of truth.
    
    In this implementation, the @ngrx/store library is used to manage the application state related to ingredients. Actions, reducers, and selectors are defined to handle adding ingredients to the state. The actions represent the different ways in which ingredients can be added, such as adding a single ingredient or adding multiple ingredients at once. The reducers handle these actions and update the state accordingly. Selectors are defined to retrieve the updated ingredient list from the state.
    
    This implementation of state management using @ngrx/store provides a scalable and maintainable solution for handling ingredient data in the application, making it easier to manage and update ingredient data across the application.
    
    Changes Made:
    
    Added actions for adding ingredients
    Implemented reducers to handle the actions and update the state
    Defined selectors to retrieve the updated ingredient list from the state
    Integrated @ngrx/store into the application for state management
    Tested and verified the functionality of adding ingredients using the new state management implementation
    Impact:
    
    Improved application state management for adding ingredients
    Centralized and organized state management using @ngrx/store
    Scalable and maintainable solution for handling ingredient data in the application
    Easier management and updating of ingredient data across the application

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -9,16 +10,17 @@ import { ShoppingListService } from './shopping-list.service';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
+  ingredients: Observable<{ingredients: Ingredient[]}>;
   private igChangeSub: Subscription;
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService, private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) { }
 
   ngOnInit(): void {
-    this.ingredients = this.slService.getIngredients();
-    this.igChangeSub = this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
-      this.ingredients = ingredients;
-    })
+    this.ingredients = this.store.select('shoppingList');
+    // this.ingredients = this.slService.getIngredients();
+    // this.igChangeSub = this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+    //   this.ingredients = ingredients;
+    // })
   }
 
   onEditItem(index: number) {
@@ -26,7 +28,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.igChangeSub.unsubscribe();
+    // this.igChangeSub.unsubscribe();
   }
 
 }
